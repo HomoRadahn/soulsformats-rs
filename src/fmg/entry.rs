68321441c66b2729b7e0c09034ade2e1844b9676
HomoRadahn <koxipro@gmail.com>
2026-09-22 12:54:27 +0200

    Added FMG, version regression

---
+++ src/fmg/entry.rs
@@ -0,0 +1,25 @@
+use core::fmt;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Entry {
+    /// The ID of this `Entry`
+    pub id: i32,
+    /// The text data of this `Entry`
+    pub text: Option<String>,
+}
+
+impl Entry {
+    /// Creates `Entry` with specified parameters
+    pub fn new(id: i32, text: Option<impl Into<String>>) -> Self {
+        Self { id, text: text.map(Into::into) }
+    }
+}
+
+impl fmt::Display for Entry {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self.text {
+            Some(text) => write!(f, "{}: {}", self.id, text),
+            None => write!(f, "{}: <null>", self.id),
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod binder;
 pub mod btl;
 pub mod dcx;
+pub mod fmg;
 pub mod io;
 mod oodle;
 pub mod util;
@@ -10,4 +11,5 @@ pub use dcx::{
     DCX,
     compression_info::{CompressionInfo, DcxDfltArgs, DcxDfltPreset, DcxKrakArgs},
 };
+pub use fmg::FMG;
 pub use io::{ByteIO, FileIO};
